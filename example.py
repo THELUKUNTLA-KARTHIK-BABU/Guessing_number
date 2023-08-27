18722214ccca985ecb28ce8051c1c7491340057e
karthik <114253896+THELUKUNTLA-KARTHIK-BABU@users.noreply.github.com>
2023-08-27 18:47:06 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,19 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================================== RESTART: C:/Users/karth/AppData/Local/Programs/Python/Python310/libs/guessing number.py =====================================
+Enter Lower bound:- 1
+Enter Upper bound:- 100
+
+	You've only  7  chances to guess the integer!
+
+Guess a number:- 42
+You guessed too small!
+Guess a number:- 55
+You Guessed too high!
+Guess a number:- 50
+You Guessed too high!
+Guess a number:- 46
+You guessed too small!
+Guess a number:- 48
+Congratulations you did it in  5  try
